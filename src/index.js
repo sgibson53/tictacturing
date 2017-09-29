4f7ce587df262f3d5686ab9f96a773dd3fed73c6
Seth Gibson <sethg@seths-mbp.attlocal.net>
2017-09-29 18:47:49 -0500

    Removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div>
+    <h1>Hello World</h1>
+</div>, document.getElementById('root'))
 registerServiceWorker();
